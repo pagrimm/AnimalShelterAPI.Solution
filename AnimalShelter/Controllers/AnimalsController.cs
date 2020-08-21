54646abb95eb7f92d0ca95b055f2d5a876ad8c6b
Peter Grimm <pagrimm@gmail.com>
2020-08-21 14:52:37 -0700

    add api versioning and swagger

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -6,39 +6,23 @@ using System.Linq;
 
 namespace AnimalShelter.Controllers
 {
-  [Route("api/[controller]")]
+  [ApiVersion("1.0")]
+  [Route("api/v1/animals")]
   [ApiController]
-  public class AnimalsController : ControllerBase
+  public class AnimalsV1Controller : ControllerBase
   {
     private AnimalShelterContext _db;
 
-    public AnimalsController(AnimalShelterContext db)
+    public AnimalsV1Controller(AnimalShelterContext db)
     {
       _db = db;
     }
 
     // GET api/animals
     [HttpGet]
-    public ActionResult<IEnumerable<Animal>> Get(string species, string gender, string name)
+    public ActionResult<IEnumerable<Animal>> Get()
     {
-      var query = _db.Animals.AsQueryable();
-
-      if (species != null)
-      {
-        query = query.Where(entry => entry.Species == species);
-      }
-
-      if (gender != null)
-      {
-        query = query.Where(entry => entry.Gender == gender);
-      }
-
-      if (name != null)
-      {
-        query = query.Where(entry => entry.Name == name);
-      }
-
-      return query.ToList();
+      return _db.Animals.ToList();
     }
 
     // POST api/animals
@@ -72,6 +56,39 @@ namespace AnimalShelter.Controllers
       var animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
       _db.Animals.Remove(animalToDelete);
       _db.SaveChanges();
+    }
+  }
+
+  [ApiVersion("2.0")]
+  [Route("api/v2/animals")]
+  [ApiController]
+  public class AnimalsV2Controller : ControllerBase
+  {
+    private AnimalShelterContext _db;
+
+    public AnimalsV2Controller(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/animals
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> Get(string species, string gender, string name)
+    {
+      var query = _db.Animals.AsQueryable();
+      if (species != null)
+      {
+        query = query.Where(entry => entry.Species == species);
+      }
+      if (gender != null)
+      {
+        query = query.Where(entry => entry.Gender == gender);
+      }
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+      return query.ToList();
     }
   }
 }
--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,15 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace AnimalShelter.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+    [Required]
+    [StringLength(20)]
     public string Name { get; set; }
+    [Required]
     public string Species { get; set; }
     public string Breed { get; set; }
+    [Required]
+    [Range(0, 200, ErrorMessage = "Age must be between 0 and 200.")]
     public int Age { get; set; }
+    [Required]
     public string Gender { get; set; }
+    [Required]
     public DateTime CheckInDate { get; set; }
     public DateTime? AdoptionDate { get; set; }
     public Boolean IsAdopted { get; set; } = false;
